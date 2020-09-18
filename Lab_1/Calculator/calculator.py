a5639ffaf852f61a9abc0b767b11e1f25cd5371d
iwilare <iwilare@gmail.com>
2020-09-18 10:18:49 +0200

    First commit

---
+++ Lab_1/Calculator/calculator.py
@@ -0,0 +1,21 @@
+def sum(m, n):
+    sign = 1 if n > 0 else -1
+    n = abs(n)
+    while n != 0:
+        m += sign
+        n -= 1
+    return m
+
+def divide(m, n):
+    i = 0
+    isNegative = m > 0 and n < 0 or m < 0 and n > 0
+    m = abs(m)
+    n = abs(n)
+
+    while m - n >= 0:
+        m -= n
+        i += 1
+
+    i = -i if isNegative else i
+
+    return i
